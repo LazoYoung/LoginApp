fd332ac1b77f3193df1791b9b0dfabb4286360d4
Chanyoung Park <starkergame@gmail.com>
2023-04-03 23:01:00 +0900

    Initial commit

---
+++ app/src/main/java/com/google/loginapp/RelativeLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.google.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relativelogin)
+    }
+}
